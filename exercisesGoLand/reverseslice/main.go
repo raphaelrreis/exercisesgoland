cb67a70c188b48de8f8bfcadccb798a9c445dc32
Raphael Reis <rapphha@gmail.com>
2021-02-09 18:55:54 -0300

    Create function of soluction.

--- exercisesGoLand/reverseslice/main.go
+++ exercisesGoLand/reverseslice/main.go
@@ -2,9 +2,20 @@ package reverseslice
 
 import "fmt"
 
+func soluction(T []int) []int {
+
+	b := []int{}
+	a := len(T)
+	for i := range T {
+		b = append(b, T[(a-i)-1])
+	}
+	return b
+
+}
+
 func main() {
 	t := []int{8, 5, 4, 2, 10, 52}
 	fmt.Print("Original ->", t)
-	fmt.Print("Reversed ->")
+	fmt.Print("Reversed ->", soluction(t))
 
 }
