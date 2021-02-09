2ed8e2a882e2e36bd4b26560555b497528c382fa
Raphael Reis <rapphha@gmail.com>
2021-02-09 18:29:37 -0300

    Returns the length of winter from a slice of temperature represented by int.

--- exercisesGoLand/main.go
+++ exercisesGoLand/main.go
@@ -2,8 +2,32 @@ package exercisesGoLand
 
 import "fmt"
 
+func soluction(T []int) int {
+
+	maxInv := T[0]
+	var indexStartSummer int
+	maxTemp := T[10]
+
+	for i := range T {
+		if maxTemp < T[i] {
+			maxTemp = T[i]
+		}
+		if maxInv >= T[i] {
+			indexStartSummer = -1
+			maxInv = maxTemp
+
+		} else {
+			if indexStartSummer == -1 {
+				indexStartSummer = i
+			}
+		}
+	}
+
+	return indexStartSummer
+}
+
 func main() {
 	t := []int{-10, 5, -12, 3, 8, 9}
-	fmt.Print("length of winter")
+	fmt.Print("length of winter", soluction(t))
 
 }
