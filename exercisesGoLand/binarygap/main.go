b7016fe8926394c33236b75c19b1e76a9eba2ae4
Raphael Reis <rapphha@gmail.com>
2021-02-10 07:54:56 -0300

    add new file.

--- exercisesGoLand/binarygap/main.go
+++ exercisesGoLand/binarygap/main.go
@@ -1,5 +1,53 @@
 package binarygap
 
+import (
+	"strconv"
+	"strings"
+)
+
+/**
+Soluction2 gets
+*/
+func Soluction2(N int) int {
+
+	n := int64(N)
+	tmp := strconv.FormatInt(n, 2)
+
+	max := 0
+	temp := 0
+	for _, r := range tmp {
+		if r == '0' {
+			temp++
+
+		} else {
+			if temp > max {
+				max = temp
+
+			}
+			temp = 0
+		}
+
+	}
+	return max
+
+}
+
+func Soluctio(N int) int {
+	n := int64(N)
+	tmp := strconv.FormatInt(n, 2)
+	s := strings.Split(tmp, "1")
+
+	max := 0
+
+	for i := range s {
+		if len(s[i]) > max {
+			max = len(s[i])
+
+		}
+	}
+	return max
+}
+
 func main() {
 
 }
